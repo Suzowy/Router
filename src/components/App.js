9b763b11e78d6a1c445eeaffb763918a00e20cbe
Suzowy <suzowy@gmail.com>
2023-05-12 09:30:19 +0200

    primera subida

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import '../styles/App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import Home from './Home/Home';
+import Contact from './Contact/Contact';
+import NotFound from './NotFound';
+import ProductDetail from './Product/ProductDetails';
+
+function App() {
+  return (
+    <div className="App-header">
+      <nav>
+        <ul className="menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            {/*Enlaces para redireccionar a las ruta definida en <Route> */}
+            <Link to="/contacto">Contacto </Link>
+          </li>
+          <li>
+            {/* Esto es una ruta dinamica, donde 123, puede ser un valor variable */}
+            <Link to="/producto/123">detalle Producto </Link>
+          </li>
+        </ul>
+      </nav>
+      {/* http://localhost:3000/producto/001*/}
+      <Routes>
+        <Route path="/" element={<Home />} />{' '}
+        {/* esta seria una ruta estática porque su valor se mantiene siempre igual*/}
+        <Route path="/contacto" element={<Contact />} />
+        <Route path="/producto/:id" element={<ProductDetail />} />{' '}
+        {/*esta ruta contiene una parte variable (:id), puedes ponerle cualquier nombre */}
+        <Route path="*" element={<NotFound />} />{' '}
+        {/* cuando se trate de acceder a una URL  que no esta definida en los <Route> se renderizara el componente Not Found que tiene un mensaje de ruta no encontrada*/}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+import img from "../images/notFound.jpg";
+
+const NotFound = () => {
+
+    return (
+        <img src={img} alt="not found page " />
+    )
+}
+
+export default NotFound;
